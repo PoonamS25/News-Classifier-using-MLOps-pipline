beef6cd8f119e72c0b3c5415837588908858ea1d
PoonamS25 <101706028+PoonamS25@users.noreply.github.com>
2022-05-06 17:56:52 +0530

    Add files via upload

--- web_scrap.py
+++ web_scrap.py
@@ -8,8 +8,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+import pymongo
 
-%matplotlib inline
+
 
 seed_urls = ['https://inshorts.com/en/read/technology',
              'https://inshorts.com/en/read/sports',
@@ -43,3 +44,36 @@ def build_dataset(seed_urls):
 news_df = build_dataset(seed_urls)
 news_df.head(10)
 news_df.news_category.value_counts()
+
+
+
+
+def scrape_quotes():
+    more_links = True
+    page = 1
+    quotes = []
+    while(more_links):
+        markup = requests.get(f'http://quotes.toscrape.com/page/{page}').text
+        soup = BeautifulSoup(markup, 'html.parser')
+        for item in soup.select('.quote'):
+            quote = {}
+            quote['text'] = item.select_one('.text').get_text()
+            quote['author'] = item.select_one('.author').get_text()
+            tags = item.select_one('.tags')
+            quote['tags'] = [tag.get_text() for tag in tags.select('.tag')]
+            quotes.append(quote)
+        next_link = soup.select_one('.next > a')
+        print(f'scraped page {page}')
+        if(next_link):
+            page += 1
+        else:
+            more_links = False
+    return quotes
+quotes = scrape_quotes()
+client = pymongo.MongoClient('your mongodb connection string')
+db = client.db.quotes
+try:
+    db.insert_many(quotes)
+    print(f'inserted {len(quotes)} articles')
+except:
+    print('an error occurred quotes were not stored to db')
