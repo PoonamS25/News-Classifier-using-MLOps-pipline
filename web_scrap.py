a03f090014d64b079737877d78d06e68308c00b3
PoonamS25 <101706028+PoonamS25@users.noreply.github.com>
2022-05-09 19:28:14 +0530

    Update web_scrap.py

--- web_scrap.py
+++ web_scrap.py
@@ -48,32 +48,7 @@ news_df.news_category.value_counts()
 
 
 
-def scrape_quotes():
-    more_links = True
-    page = 1
-    quotes = []
-    while(more_links):
-        markup = requests.get(f'http://quotes.toscrape.com/page/{page}').text
-        soup = BeautifulSoup(markup, 'html.parser')
-        for item in soup.select('.quote'):
-            quote = {}
-            quote['text'] = item.select_one('.text').get_text()
-            quote['author'] = item.select_one('.author').get_text()
-            tags = item.select_one('.tags')
-            quote['tags'] = [tag.get_text() for tag in tags.select('.tag')]
-            quotes.append(quote)
-        next_link = soup.select_one('.next > a')
-        print(f'scraped page {page}')
-        if(next_link):
-            page += 1
-        else:
-            more_links = False
-    return quotes
-quotes = scrape_quotes()
-client = pymongo.MongoClient('your mongodb connection string')
-db = client.db.quotes
-try:
-    db.insert_many(quotes)
-    print(f'inserted {len(quotes)} articles')
-except:
-    print('an error occurred quotes were not stored to db')
+conn = pymongo.MongoClient('mongodb://localhost:27017')
+db = conn["database"]
+db.myTable.insert_many(news_df.to_dict("records"))
+print(f'inserted {len(news_df)} articles')
