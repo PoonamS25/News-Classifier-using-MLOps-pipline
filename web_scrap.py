905c09f5d4d2f1b4d50158bc8767e5b773e12268
PoonamS25 <101706028+PoonamS25@users.noreply.github.com>
2022-05-17 18:49:16 +0530

    Update web_scrap.py

--- web_scrap.py
+++ web_scrap.py
@@ -1,6 +1,4 @@
 
-
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -45,10 +43,14 @@ news_df = build_dataset(seed_urls)
 news_df.head(10)
 news_df.news_category.value_counts()
 
-
-
-
+news_df.reset_index(inplace=True)
+data_dict = news_df.to_dict()
+print(data_dict)
 conn = pymongo.MongoClient('mongodb://localhost:27017')
 db = conn["database"]
 db.myTable.insert_many(news_df.to_dict("records"))
 print(f'inserted {len(news_df)} articles')
+col = db["myTable"]
+
+
+
